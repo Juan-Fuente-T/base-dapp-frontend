02bbca5bc49bb1c92fac01cbc209d12177f2ed2f
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-07-17 14:14:44 +0200

    WIP: Logra el mint pero no esta permitiendo el transfer

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 // export const CONTRACT_ADDRESS = '0x48177f98d8Bab403c5B339370515A8b6b6b62A1B'
-export const CONTRACT_ADDRESS = '0xeE054da1f69EEB4953e4757Ad0b7Fd006a53b58C'
+// export const CONTRACT_ADDRESS = '0xeE054da1f69EEB4953e4757Ad0b7Fd006a53b58C'//1155 sencillo, sin autentication ni onlyowner
+// export const CONTRACT_ADDRESS = '0x73c6eA92eff63f81D9B9D3F5A0f2EB8b4d01D364' //1155 con autentication , sin onlyowwer
+export const CONTRACT_ADDRESS = '0xC5bCED224eB95785cCc6b2836A16bC3FCA7A087A' //1155 con autentication , sin onlyowwer en ArbitrumSepolia
