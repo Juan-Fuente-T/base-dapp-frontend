023bb74ecc95172bc874e2f3a7af5d5bd4d38c3f
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-07-29 22:32:28 +0200

    Wip: No esta mostrando el balance actualizado para EducatETH. Mintea y transfiere bien.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,6 @@
 // export const CONTRACT_ADDRESS = '0x73c6eA92eff63f81D9B9D3F5A0f2EB8b4d01D364' //1155 con autentication , sin onlyowwer
 
 // export const CONTRACT_ADDRESS = '0xC5bCED224eB95785cCc6b2836A16bC3FCA7A087A' //1155 con autentication , sin onlyowwer en ArbitrumSepolia
-export const CONTRACT_ADDRESS = '0x5BC4F8626E434F3f209Bcb7fB002e7003716e586' //1155 sin autentication , sin onlyowwer en Sepolia para BusinessCard
+// export const CONTRACT_ADDRESS = '0x5BC4F8626E434F3f209Bcb7fB002e7003716e586' //1155 sin autentication , sin onlyowwer en Sepolia para BusinessCard
+
+export const CONTRACT_ADDRESS = '0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B' //1155 con autentication , sin onlyowwer en Sepolia
